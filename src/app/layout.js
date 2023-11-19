6ea8a658ee93a5c5c7a5217ba7c7eea035789339
unknown <abdul.m0701@gmail.com>
2023-11-18 19:03:44 -0500

    page sections added to nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,12 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Abdul's Portfolio",
-  description: 'Developed by Abdul',
+  title: "Home",
+  description: "Abdul's Portfolio",
 }
 
 export default function RootLayout({ children }) {
+  console.log(children);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,20 @@
 import Image from 'next/image';
-import Head from "next/head";
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 export default function Home() {
 
   return (
     <div> 
-      <Head>
-        <title>Abdul Mahmoud Portfolio</title>
-        <meta name="description" content="Abdul's Personal Portfolio"/>
-        <link ref="icon" href='/favicon.ico'/>
-      </Head>
       <main className="bg-orange-50 px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-2xl font-semibold">devdul.me</h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill className="cursor-pointer text-2xl mx-10" /></li>
+              <li>About Me</li>
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact</li>
+              <li><BsFillMoonStarsFill className="cursor-pointer text-2xl " /></li>
               <li><a className="bg-gradient-to-r from-red-900 to-red-950 text-white p-2.5 rounded-lg" href='#'>Resume</a></li>
             </ul>
           </nav>
