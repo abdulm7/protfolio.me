8c2db1b86e49e752a28bfd2c8ce5e06872a20b07
unknown <abdul.m0701@gmail.com>
2023-11-20 00:07:29 -0500

    Update: tab title name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Home",
+  title: "Abdul | Portfolio",
   description: "Abdul's Portfolio",
 }
 
